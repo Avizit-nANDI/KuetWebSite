4cccb48208f676fd88f5fbdc534af04c342247c2
Avizit-nANDI <avizitnandi1999@gamail.com>
2020-10-12 00:20:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kuetwebsite/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.kuetwebsite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webViewId);
+        webView.loadUrl("http://www.kuet.ac.bd/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.kuet.ac.bd/");
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
